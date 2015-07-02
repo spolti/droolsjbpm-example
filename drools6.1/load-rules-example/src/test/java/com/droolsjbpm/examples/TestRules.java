5d31018347facc9cfafb4d51badbccd567f0d762
spolti <filippespolti@gmail.com>
2015-07-02 18:09:20 -0300

    adding projects

---
+++ drools6.1/load-rules-example/src/test/java/com/droolsjbpm/examples/TestRules.java
@@ -0,0 +1,90 @@
+package com.droolsjbpm.examples;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.drools.core.io.impl.UrlResource;
+import org.junit.Test;
+import org.kie.api.KieBase;
+import org.kie.api.KieServices;
+import org.kie.api.builder.KieModule;
+import org.kie.api.builder.KieRepository;
+import org.kie.api.builder.KieScanner;
+import org.kie.api.builder.ReleaseId;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+public class TestRules {
+
+	private Person p = new Person();
+
+	@Test
+	public void embedded() throws Exception {
+		System.out.println("Using embedded approach.");
+
+		KieServices ks = KieServices.Factory.get();
+		KieContainer kContainer = ks.getKieClasspathContainer();
+
+		// this session name is declared in the kmodule.xm file
+		KieSession session = kContainer.newKieSession("ksession-rules");
+
+		p.setName("User");
+		session.insert(p);
+		session.fireAllRules();
+		session.dispose();
+	}
+
+	@Test
+	public void kieScanner() throws InterruptedException {
+		System.out.println("Using kieScanner approach.");
+
+		// load the jar from .m2
+		System.setProperty("kie.maven.settings.custom", "/home/user/.m2/settings.xml");
+		KieServices kieServices = KieServices.Factory.get();
+
+		ReleaseId releaseId = kieServices.newReleaseId("example", "example-load-rules", "LATEST");
+		//ReleaseId releaseId = kieServices.newReleaseId("test", "test", "LATEST");
+		KieContainer kContainer = kieServices.newKieContainer(releaseId);
+
+		KieScanner kScanner = kieServices.newKieScanner(kContainer);
+
+		kScanner.scanNow();
+
+		KieBase kbase = kContainer.getKieBase();
+		KieSession ksession = kbase.newKieSession();
+		System.out.println("version: " + kContainer.getReleaseId().getVersion());
+
+		p.setName("User");
+		ksession.insert(p);
+		ksession.fireAllRules();
+
+	}
+
+	@Test
+	public void fromUrl() throws IOException {
+		System.out.println("Using BC's maven URL approach.");
+
+		String ARTIFACT_URL = "http://localhost:8080/business-central/maven2/example/example-load-rules/12/load-rules-12.jar";
+
+		KieServices ks = KieServices.Factory.get();
+		KieRepository kr = ks.getRepository();
+		UrlResource urlResource = (UrlResource) ks.getResources().newUrlResource(ARTIFACT_URL);
+		urlResource.setUsername("brmsAdmin");
+		urlResource.setPassword("Kt@25m69");
+		urlResource.setBasicAuthentication("enabled");
+
+		InputStream is = urlResource.getInputStream();
+
+		KieModule kModule = kr.addKieModule(ks.getResources().newInputStreamResource(is));
+		KieContainer kContainer = ks.newKieContainer(kModule.getReleaseId());
+
+		KieBase kbase = kContainer.getKieBase();
+		KieSession ksession = kbase.newKieSession();
+
+		p.setName("User");
+		ksession.insert(p);
+		ksession.fireAllRules();
+		ksession.dispose();
+
+	}
+}
