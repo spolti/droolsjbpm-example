cd892edff78d36f27831ad576769a1490afda8ba
spolti <filippespolti@gmail.com>
2015-09-11 14:56:53 -0300

    removing password

--- jbpm6.2/start-process-bpms61/src/main/java/com/droolsjbpm/examples/configuration/ServerInformation.java
+++ jbpm6.2/start-process-bpms61/src/main/java/com/droolsjbpm/examples/configuration/ServerInformation.java
@@ -4,8 +4,8 @@ public enum ServerInformation {
 
 	DEPLOYMENT_ID("fspolti:fspolti-test:1.0"),
 	SERVER_URL("http://localhost:8080/business-central"),
-	USER("fspolti"),
-	PASSWORD("Kt@25m69");
+	USER("USER"),
+	PASSWORD("PASSWD");
 	
 	private String ServerInformation;
 		
